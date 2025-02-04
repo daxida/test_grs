17149edd66034c84e28183420fad77fee6c43dd3
daxida <galmaw@hotmail.com>
2025-02-04 10:51:10 +0100

    fixing stuff

---
+++ www/pkg/test_grs_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const check_spelling: (a: number, b: number) => number;
+export const log_message: (a: number, b: number) => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
