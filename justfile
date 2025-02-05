cc1de8a05934ebba106ee6eadb87944d93db0d2a
daxida <galmaw@hotmail.com>
2025-02-05 22:45:16 +0100

    Use `grs` in `scan_text`

--- justfile
+++ justfile
@@ -1,2 +1,5 @@
 build:
   wasm-pack build --target web
+
+server:
+  python3 -m http.server 8080
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
+use grs::linter::check;
+use grs::registry::Rule;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 use web_sys::js_sys::Array;
 
 #[wasm_bindgen]
 pub fn scan_text(text: &str) -> Array {
-    let errors = Array::new();
-    let target = "bad";
+    let config = vec![Rule::MissingDoubleAccents];
+    let diagnostics = check(text, &config);
 
-    for (i, _) in text.match_indices(target) {
+    let errors = Array::new();
+    for diagnostic in diagnostics {
         let error_range = Array::new();
-        error_range.push(&JsValue::from(i));
-        error_range.push(&JsValue::from(i + target.len()));
+        if let Some(fix) = diagnostic.fix {
+            let range = fix.range;
+            error_range.push(&JsValue::from(range.start()));
+            error_range.push(&JsValue::from(range.end()));
+        }
         errors.push(&error_range);
     }
 
